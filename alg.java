7f49915b9dd7f55a3c19458f8696026fee87b2f7
Kaihzz <psampaiodasilva8@gmail.com>
2026-03-03 17:38:04 -0400

    Update alg.java

--- alg.java
+++ alg.java
@@ -1,24 +1,24 @@
+
 //Pedro Henrique Sampaio da Silva :-:
 import java.util.Scanner;
 public class alg {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);//inicia Scanner
     byte oP = 0; //opção
-    double lD1 = 0; // lado 1
-    double lD2 = 0; // lado 2 
-    double lD3 = 0; // lado 3 (caso do triangulo)
+    double ld1 = 0; // lado 1
+    double ld2 = 0; // lado 2 
+    double ld3 = 0; // lado 3 (caso do triangulo)
     double al = 0;// altura do triangulo
     double bs = 0;// base do triangulo
-    double pR = 0;//perimetro
-    double aR = 0;//area 
-    double pI = 3.14;
+    double pr = 0;//perimetro
+    double ar = 0;//area
     boolean c = true;
     //inicio do programa
     System.out.print(" # Escolha a forma geométrica\n\n");
     System.out.println("  1. Retângulo;\n  2. Círculo;\n  3. Quadrado;\n  4. Triângulo;\n  5. Fechar Programa;");
 
     //exibe o menu e inicia a repetição por while
-while( c == true){
+while(c){
     System.out.print("\n\n Insira a opção: ");
     oP = sc.nextByte();
 
@@ -27,32 +27,32 @@ while( c == true){
             //calculo de Retângulo
             System.out.println("\n Você escolheu a opção retângulo");
             System.out.print("\n  Digite a altura: ");
-            lD1 = sc.nextDouble();
+            ld1 = sc.nextDouble();
             System.out.print("  Digite o comprimento: ");
-            lD2 = sc.nextDouble();
-            aR = lD1 * lD2;
-            pR = (lD1 * 2) + (lD2 * 2);
-            System.out.printf("\n A área do retângulo é %,.2f e o perimetro é %,.1f. ", aR, pR);
+            ld2 = sc.nextDouble();
+            ar = ld1 * ld2;
+            pr = (ld1 * 2) + (ld2 * 2);
+            System.out.printf("\n A área do retângulo é %,.2f e o perimetro é %,.1f. ", ar, pr);
             break;
         }
         case 2:{
             //calculo do Circulo
             System.out.println("\n Você escolheu a opção círculo");
             System.out.print("\n  Digite o raio do círculo: ");
-            lD1 = sc.nextDouble();
-            aR = lD1 * lD1 * pI;
-            pR = (pI * 2) * lD1;
-            System.out.printf("\n A área do círculo é %,.2f e o perimetro é %,.1f ", aR, pR);
+            ld1 = sc.nextDouble();
+            ar = ld1 * ld1 * Math.PI;
+            pr = (Math.PI * 2) * ld1;
+            System.out.printf("\n A área do círculo é %,.2f e o perimetro é %,.1f ", ar, pr);
             break;
         }
         case 3:{
             //calculo do Quadrado
             System.out.println("\n Você escolheu a opção quadrado");
             System.out.print("\n  Digite o lado: ");
-            lD1 = sc.nextDouble();
-            aR = lD1 * lD1;
-            pR = lD1 * 4;
-            System.out.printf("\n A área do quadrado é %,.2f e o perimetro é %,.1f. ", aR, pR);
+            ld1 = sc.nextDouble();
+            ar = ld1 * ld1;
+            pr = ld1 * 4;
+            System.out.printf("\n A área do quadrado é %,.2f e o perimetro é %,.1f. ", ar, pr);
             break;
         }
         case 4:{
@@ -63,14 +63,14 @@ while( c == true){
             System.out.print("  Digite a base: ");
             bs = sc.nextDouble();
             System.out.print("  Digite o lado 1: ");
-            lD1 = sc.nextDouble();
+            ld1 = sc.nextDouble();
             System.out.print("  Digite o lado 2: ");
-            lD2 = sc.nextDouble();
+            ld2 = sc.nextDouble();
             System.out.print("  Digite o lado 3: ");
-            lD3 = sc.nextDouble();
-            aR = ((al * bs) / 2);
-            pR = lD1 + lD2 + lD3;
-            System.out.printf("\n  A área do triângulo é %,.2f e o perimetro é %,.1f ", aR, pR);
+            ld3 = sc.nextDouble();
+            ar = ((al * bs) / 2);
+            pr = ld1 + ld2 + ld3;
+            System.out.printf("\n  A área do triângulo é %,.2f e o perimetro é %,.1f ", ar, pr);
             break;
         }
         case 5:{
